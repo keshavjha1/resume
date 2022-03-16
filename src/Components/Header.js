0889e3b2f8a175a3b4a52af301e92a58cd7dba9b
keshav <keshavjha68@gmail.com>
2022-03-16 12:09:12 +0530

    changing particle size

--- src/Components/Header.js
+++ src/Components/Header.js
@@ -13,7 +13,7 @@ class Header extends Component {
 
     return (
       <header id="home">
-        <ParticlesBg type="polygon" bg={true} />
+        <ParticlesBg color="#ff0000" type="color" num={2} bg={true} />
 
         <nav id="nav-wrap">
           <a className="mobile-btn" href="#nav-wrap" title="Show navigation">
